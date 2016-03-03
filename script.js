8d50deb1789bf57f7e795ea74332059166f43c38
shivam <shivam.bitsgoa@gmail.com>
2016-03-03 18:24:31 +0800

    init

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).on("pageinit", '#roles-page', function(){
+	
+	RoleUtils.loadDataIntoView();
+
+	$('#addNewRoleForm').on('submit', function(e){
+		var data = $("#addNewRoleForm :input").serializeArray();
+		var roleName = data[0].value;
+		RoleUtils.addNewRole(roleName);
+	});
+});
+
+/*
+
+$(document).on("pageinit", '#scopes-page', function(){
+	ScopeUtils.loadDataIntoView();
+});
+
+$(document).on("pageinit", '#devices-page', function(){
+
+});
+*/
+
+$(document).on("pagebeforeshow","#scopes-page",function(){ 
+	$('#scopes-header').remove();
+	$('#scopes-list').empty();
+});
+
+$(document).on("pageshow","#scopes-page",function(){ 
+	ScopeUtils.loadDataIntoView();
+});
